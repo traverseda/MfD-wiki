209c2097071b06600067e4b1fc57d32a9f0e7f37
traverseda <traverse.da@gmail.com>
2017-09-03 17:11:06 -0300

    Removed comments.

--- Settings/settings.py
+++ Settings/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django_comments',
+#    'django_comments', #We'll do comments later, maybe.
     'reversion',
     'taggit',
     'core',
